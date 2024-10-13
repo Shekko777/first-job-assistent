dddf0738e0413c06f5474dcfed04b0847be1e9f3
Ivan Pershin <shekko-777@yandex.ru>
2024-10-13 14:50:18 +0300

    feat Первый комм

---
+++ index.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('.form');
+const inputTitle = document.querySelector('.form__input-title');
+const inputText = document.querySelector('.form__input-text');
+const arr = localStorage.getItem('arr') || [];
+
+document.onload = function () {
+  console.log(window.localStorage.getItem('arr'))
+}()
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  arr.push(inputTitle.value);
+  localStorage.setItem('arr', );
+  console.log(arr)
+});
